a15c0b73ba54ef064d4290bccac28f08f7ec1ab9
Adarshrajsrivastwa <srivastwaadarsh@gmail.com>
2025-08-14 18:51:39 +0530

    update navbar

--- DigitalGenZ/src/pages/DigitalZenze.jsx
+++ DigitalGenZ/src/pages/DigitalZenze.jsx
@@ -71,7 +71,7 @@ const DigitalZenze = () => {
       <img
         src="https://res.cloudinary.com/dlk5kntmy/image/upload/v1755083075/logo-removebg-preview_xljgwo.png"
         alt="Digital GenZ"
-        className="h-{10vh} w-auto"
+        className="h-{2vh} w-auto"
       />
     </div>
 
